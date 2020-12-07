9b4b1948dd27424a9b285bfb6f6f3c5c0b9e3bf7
withluaan <leminh2kz46@gmail.com>
2020-12-07 16:42:58 +0700

    add model GheModel, LichChieuModel, PhongChieuModel, VeModel

---
+++ Doan/Models/GheModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doan.Areas.Admin.Models
+{
+    public class GheModel
+    {   //mã ghế
+        [Key]
+        public int maghe { get; set; }
+        //mã phòng
+        [Required]
+        public int maphong { get; set; }
+        [ForeignKey("maphong")]
+        public virtual PhongChieuModel phongchieu { get; set; }
+        //giá
+        public int gia { get; set; }
+        //trạng thái
+        public int trangthai { get; set; }
+    }
+}
