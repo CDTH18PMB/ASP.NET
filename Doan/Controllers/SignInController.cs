03dcbd3992d3c487afaf764dc187223841f4fc25
ngophong030700 <0306181351@caothang.edu.vn>
2020-12-27 20:52:42 +0700

    Them chuc nang dang ki va thong bao loi

--- Doan/Controllers/SignInController.cs
+++ Doan/Controllers/SignInController.cs
@@ -35,6 +35,8 @@ namespace Doan.Controllers
              StringProcessing.CreateMD5Hash(taikhoan.Password))).ToList();
             if (r.Count == 0)
             {
+                string message = "Tài khoản hoặc mật khẩu không đúng.";
+                SetAlert(message, 2);
                 return View("Index");
             }
             //gắn session
@@ -46,6 +48,46 @@ namespace Doan.Controllers
                 return RedirectToAction("Index", "Admin", new { area = "Admin" });
             }
             return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Register([Bind("Username,Password")] TaiKhoanModel taikhoan)
+        {
+            var check = _context.TaiKhoan.FirstOrDefault(s => s.Username == taikhoan.Username);
+            if (check==null)
+            {
+                taikhoan.Password = StringProcessing.CreateMD5Hash(taikhoan.Password);
+                taikhoan.LoaiTK = "Member";
+                _context.TaiKhoan.Add(taikhoan);
+                _context.SaveChanges();
+                string message = "Đăng ký thành công.";
+                SetAlert(message,1);
+                return RedirectToAction("Sign_Up", "SignIn");
+            }
+            else
+            {
+                string message = "Email đã tồn tại.";
+                SetAlert(message, 2);
+                return View("Sign_Up");
+            }
+        }
+        protected void SetAlert(string message, int type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == 1)
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == 2)
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == 3)
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+            else
+            {
+                TempData["AlertType"] = "alert-info";
+            }
         }
     }
 }
