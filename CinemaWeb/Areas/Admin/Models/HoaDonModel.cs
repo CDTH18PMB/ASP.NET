db048330b1f8544e67837b54c74cfa079611e8a3
Hoàng Lâm <vuhoanglam2000vn@gmail.com>
2021-01-15 00:44:51 +0700

    mere Lam + Luan + Phong. P2

---
+++ CinemaWeb/Areas/Admin/Models/HoaDonModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWeb.Areas.Admin.Models
+{
+    public class HoaDonModel
+    {
+        [Key]
+        public int MaHD { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+        [ForeignKey("Username")]
+        public virtual TaiKhoanModel username { get; set; }
+
+        [Required]
+        [Column(TypeName ="nvarchar(100)")]
+        public string GheDaDat { get; set; }
+
+        [Required]
+        public int TongTien { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Ngay { get; set; }
+
+        [Required]
+        public bool TrangThai { get; set; }
+
+        public HoaDonModel()
+        {
+
+        }
+
+        public HoaDonModel(string username, string ghedadat, int tongtien, DateTime ngay, bool trangthai)
+        {
+            this.Username = username;
+            this.GheDaDat = ghedadat;
+            this.TongTien = tongtien;
+            this.Ngay = ngay;
+            this.TrangThai = trangthai;
+        }
+    }
+}
---
+++ CinemaWeb/Controllers/SignInController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using CinemaWeb.Data;
+using CinemaWeb.Areas.Admin.Models;
+using CinemaWeb.Areas.Admin.Data;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+
+namespace CinemaWeb.Controllers
+{
+    public class SignInController : Controller
+    {
+        private readonly DPContext _context;
+        public SignInController(DPContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sign_Up()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login([Bind("Username,Password,HoTen,SDT,LoaiTK,TrangThai")] TaiKhoanModel taikhoan)
+        {
+            if (taikhoan.Username != null && taikhoan.Password != null)
+            {
+                var r = _context.TaiKhoan.Where(m => (m.Username == taikhoan.Username && m.Password ==
+                 StringProcessing.CreateMD5Hash(taikhoan.Password))).ToList();
+                if (r.Count == 0)
+                {
+                    string message = "Tài khoản hoặc mật khẩu không đúng.";
+                    SetAlert(message, 2);
+                    return RedirectToAction("Index", "SignIn");
+                }
+                else
+                {
+                    var session = JsonConvert.SerializeObject(taikhoan);
+                    HttpContext.Session.SetString("user", session);
+                    if (r[0].LoaiTK == "Admin")
+                    {
+                        return RedirectToAction("Index", "Admin", new { area = "Admin" });
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                string message = "Tài khoản và mật khẩu không được để trống.";
+                SetAlert(message, 3);
+                return RedirectToAction("Index", "SignIn");
+            }    
+        }
+        [HttpPost]
+        public IActionResult Register([Bind("Username,Password")] TaiKhoanModel taikhoan)
+        {
+            if (taikhoan.Username != null && taikhoan.Password != null)
+            {     
+                var check = _context.TaiKhoan.FirstOrDefault(s => s.Username == taikhoan.Username);
+                if (check == null)
+                {
+                    taikhoan.Password = StringProcessing.CreateMD5Hash(taikhoan.Password);
+                    taikhoan.LoaiTK = "Member";
+                    taikhoan.HoTen = "Mặc định";
+                    taikhoan.SDT = "084";
+                    taikhoan.TrangThai = true;
+                    _context.TaiKhoan.Add(taikhoan);
+                    _context.SaveChanges();
+                    string message = "Đăng ký thành công.";
+                    SetAlert(message, 1);
+                    return RedirectToAction("Sign_Up", "SignIn");
+                }
+                else
+                {
+                    string message = "Username đã tồn tại.";
+                    SetAlert(message, 2);
+                    return RedirectToAction("Sign_Up", "SignIn");
+                }
+            }
+            else
+            {
+                string message = "Tài khoản và mật khẩu không được để trống.";
+                SetAlert(message, 3);
+                return RedirectToAction("Sign_Up", "SignIn");
+            }
+        }
+        protected void SetAlert(string message, int type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == 1)
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if (type == 2)
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if (type == 3)
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+            else
+            {
+                TempData["AlertType"] = "alert-info";
+            }
+        }
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "SignIn");
+        }
+
+    }
+}
